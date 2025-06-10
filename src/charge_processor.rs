86910c537f69963fc58780f7123e8b25a2b8a0f9
Luis Vinicius <luisvpersy@gmail.com>
2025-06-10 14:51:05 -0300

    clear the sheet and add try_until_works

--- src/charge_processor.rs
+++ src/charge_processor.rs
@@ -6,7 +6,7 @@ use crate::utils::api::sheets::client::SheetClient;
 use crate::utils::api::omie::omie_structs::Charge;
 use crate::utils::functions::clear_terminal::clear_terminal;
 use crate::utils::functions::process_single_charge::process_single_charge;
-
+use crate::utils::functions::try_until_wokrs::try_until_works;
 pub struct ChargeProcessor {
     omie_client: OmieClient,
     asaas_client: AsaasClient,
@@ -23,20 +23,19 @@ impl ChargeProcessor {
     }
 
     pub async fn process_overdue_charges(&self) -> Result<(), Box<dyn std::error::Error>> {        
-        let sheet_table = self.get_registered_sheet().await?;
         let overdue_charges = self.omie_client.list_overdue_charges().await?;
         println!("{}", overdue_charges.len());
-        
 
-        println!("{}", overdue_charges.len());
-        
-        self.process_charges_to_sheet(sheet_table, overdue_charges).await;
+            self.process_charges_to_sheet(overdue_charges).await;
         
         Ok(())
     }
 
-    async fn process_charges_to_sheet(&self, sheet_table: Vec<Vec<String>>, overdue_charges: Vec<Charge>) -> () {
-        let mut table = Vec::new();
+    async fn process_charges_to_sheet(&self, overdue_charges: Vec<Charge>) -> () {
+        let _ = try_until_works(|| async { self.sheet_client.clear_sheet().await }).await;
+        
+        
+        let mut table: Vec<Vec<String>> = Vec::new();
         let count_charges = overdue_charges.len();
 
         let mut counter = 0;
@@ -48,16 +47,19 @@ impl ChargeProcessor {
             clear_terminal();
             println!("Processando cobrança {}/{}", counter_charges, count_charges);
 
-            // Log para verificar se a cobrança já está registrada
-            if self.is_charge_registered(&charge, &sheet_table) {
-                println!("Cobrança já registrada: {:?}", charge.cNumeroContrato);
-                continue;
-            }
-
             // Log para depurar o processamento de uma cobrança
             println!("Processando cobrança com contrato: {:?}", charge.cNumeroContrato);
 
-            let row: Result<Vec<String>, Box<dyn Error + 'static>> = process_single_charge(&self.omie_client, &self.asaas_client, charge).await;
+            let row: Result<Vec<String>, Box<dyn Error + 'static>> = 
+                try_until_works(|| {
+                    async { 
+                        process_single_charge(
+                        &self.omie_client, 
+                        &self.asaas_client, 
+                        charge.clone()
+                    ).await 
+                }
+            }).await;
 
             match row {
                 Ok(row) => {
@@ -66,7 +68,7 @@ impl ChargeProcessor {
 
                     if counter >= 10 || counter_charges == count_charges {
                         println!("Enviando lote de cobranças para a planilha...");
-                        let _ = self.sheet_client.append_sheet(&table).await;
+                        let _ = try_until_works(|| async { self.sheet_client.append_sheet(&table).await }).await;
                         table.clear();
                         counter = 0;
                     }
@@ -76,32 +78,6 @@ impl ChargeProcessor {
                     continue;
                 }
             }
-        }
-    }
-
-    async fn get_registered_sheet(&self) -> Result<Vec<Vec<String>>, Box<dyn std::error::Error>> {
-        match self.sheet_client.get_sheet().await {
-            Ok(sheet) if !sheet.is_empty() => Ok(sheet),
-            Ok(_) => {
-                println!("Planilha registrada está vazia. Continuando...");
-                Ok(Vec::new())
-            }
-            Err(e) => {
-                eprintln!("Erro ao obter a planilha registrada: {}", e);
-                Ok(Vec::new())
-            }
-        }
-    }
-
-    fn is_charge_registered(&self, charge: &Charge, registered_sheet: &[Vec<String>]) -> bool {
-        if let Some(c_numero_contrato) = &charge.cNumeroContrato {
-            registered_sheet.iter().any(|row| 
-                row[1] == *c_numero_contrato &&
-                row[3] == charge.data_vencimento.to_string() &&
-                row[4] == charge.valor_documento.to_string()
-            )
-        } else {
-            false
         }
     }
 }
